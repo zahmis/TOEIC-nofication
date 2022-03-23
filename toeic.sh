9ce9ef0c9b1844d10a7f120ceb3842589d7a4fa4
zahmis <meda829ratteru86@gmail.com>
2022-03-24 04:08:28 +0900

    temp

--- toeic.sh
+++ toeic.sh
@@ -4,7 +4,12 @@ set -eu
 START=`date +%s`
 END=`(date --date '2022/05/29' +%s)`
 isAfter=$((END < START))
-restSeconds=$((isAfter && (START - END) || (END - START)))
+# restSeconds=$((isAfter && (START - END) || (END - START)))
+if(($isAfter)); then
+    echo "2022/05/29まであと$((END - START))秒"
+else
+    echo "2022/05/29まであと$((START - END))秒"
+fi
 echo ${START}
 echo ${END}
 echo ${restSeconds}
