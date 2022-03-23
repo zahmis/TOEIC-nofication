3b3ea3b4b82545cc9b76f999d6026dfe89ee12f7
zahmis <meda829ratteru86@gmail.com>
2022-03-24 04:05:18 +0900

    fix

--- toeic.sh
+++ toeic.sh
@@ -5,7 +5,7 @@ START=`date +%s`
 END=`(date --date '2022/05/29' +%s)`
 isAfter=$((END < START))
 restSeconds=$((isAfter && (START - END) || (END - START)))
-echo restSeconds
+echo ${restSeconds}
 
 slackData () {
   diffDays="$((${restSeconds} / (60 * 60 * 24)))"
